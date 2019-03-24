42606414f483d1ecd9a7274bb6e820a02e858977
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-03-24 17:31:04 +0900

    URLSessionを使って画像URLから画像を取得しimageViewにセットする

--- testKingfisher/testKingfisher/ViewController.swift
+++ testKingfisher/testKingfisher/ViewController.swift
@@ -14,18 +14,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.showImage(imageView: imageView, url: "https://o.aolcdn.com/images/dims?resize=2000%2C2000%2Cshrink&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-03%2F17a673a0-4d14-11e9-a77f-c09a6c680f31&client=a1acac3e1b3290917d92&signature=ab5e9583dc50d6e43a5f54ba50b22eec11c35102")
+        self.showImage(imageView: imageView, imageUrl: "https://o.aolcdn.com/images/dims?resize=2000%2C2000%2Cshrink&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-03%2F17a673a0-4d14-11e9-a77f-c09a6c680f31&client=a1acac3e1b3290917d92&signature=ab5e9583dc50d6e43a5f54ba50b22eec11c35102")
     }
     
-    private func showImage(imageView: UIImageView, url: String) {
-        let url = URL(string: url)
-        do {
-            let data = try Data(contentsOf: url!)
-            let image = UIImage(data: data)
-            imageView.image = image
-        } catch let err {
-            print("Error: \(err.localizedDescription)")
+    private func showImage(imageView: UIImageView, imageUrl: String) {
+        let url = URL(string: imageUrl)!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, responds, error in
+            // リクエストに失敗
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data, let response = responds as? HTTPURLResponse else {
+                return
+            }
+            
+            // リクエスト成功
+            if response.statusCode == 200 {
+                DispatchQueue.main.async {
+                    imageView.image = UIImage(data: data)
+                }
+            } else {
+                // サーバー側でリクエストされたものが正常に返せていない
+                print(response.statusCode)
+            }
         }
+        task.resume()
     }
 }
-
